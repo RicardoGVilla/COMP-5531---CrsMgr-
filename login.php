77fcb55c756f34416f0b8b121cb72ca46c5615ae
paul1n4 <paulinevalerc@gmail.com>
2024-03-24 18:58:43 -0400

    Change login css

--- login.php
+++ login.php
@@ -55,26 +55,48 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <title>CGA System</title>
     <link rel="stylesheet" type="text/css" href="css/login.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
 </head>
 <body>
-    <h2>Welcome to CrsMgr+ -- Please Login</h2>
-    <div id="login-form" class="form-body">
-        <form id="login-form" class="login-email" action="login.php" method="post">
-            <div class="input-group">
-                <input id="email" name="email" type="email" placeholder="Email Address" required>
+    <div class="login-container">
+        <div class="container">
+            <div class="header">
+                <div class="text">
+                    <h1>Welcome to CrsMgr</h1>
+                    <h2>-- The Course Manager System! --</h2>
+                </div>
             </div>
-            <div class="input-group">
-                <input id="password" name="password" type="password" placeholder="Password" required> 
+            <div class="form-container">
+                <form id="login-form" action="login.php" method="post">
+                    
+                    <div class="inputs">
+                    <div class="input-container">
+                        <img src="" alt="" />
+                        <input id="email" type="email" name='email' placeholder='Email' required/>
+                    </div>
+                    </div>
+                    <div class="inputs">
+                    <div class="input-container">
+                        <img src="" alt="" />
+                        <input id="password" type="password" name='password' placeholder='Password' required/>
+                    </div>
+                    </div>
+                    <div class="submit-container">
+                        <button class="submit" name="submit" type="submit">Login</button>
+                    </div>
+                    <div class="forgot-password">Forgot Password? <span>Click here</span></div>
+                
+                    <?php
+                    if (isset($_GET["error"]) && $_GET["error"] === "invalid_credentials") {
+                        echo "<p style='color: red;'>Invalid email or password. Please try again.</p>";
+                    }
+                    ?>
+                </form>
             </div>
-            <div class="input-group">
-                <button class="btn" name="submit" type="submit">Login</button>
-            </div>
-            <?php
-            if (isset($_GET["error"]) && $_GET["error"] === "invalid_credentials") {
-                echo "<p style='color: red;'>Invalid email or password. Please try again.</p>";
-            }
-            ?>
-        </form>
+        </div>
     </div>
+    
 </body>
 </html>
