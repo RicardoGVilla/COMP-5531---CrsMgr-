bfa28d667100f9b0403366ad76dfb3d04acf0972
paul1n4 <paulinevalerc@gmail.com>
2024-03-30 23:41:10 -0400

    Add logout path to manage_faqs and student_groups

--- users/instructor/manage_faqs.php
+++ users/instructor/manage_faqs.php
@@ -246,7 +246,7 @@
 
         <footer class="footer">
             <button onclick="location.href='../home.php'">Home</button>
-            <button onclick="location.href='logout.php'">Logout</button>
+            <button onclick="location.href='../../logout.php'">Logout</button>
         </footer>
     </div>
 
