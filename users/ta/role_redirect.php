79ff87aee01a1f6c844416070d7721ab0df13fbc
paul1n4 <paulinevalerc@gmail.com>
2024-04-10 13:40:46 -0400

    Add styles to TA and student

--- users/ta/role_redirect.php
+++ users/ta/role_redirect.php
@@ -3,16 +3,16 @@
 if (isset($_POST['role'])) {
     if ($_POST['role'] == 'TA') {
         // Redirect to the TA home page
-        header('Location: /Final_Assignment_CrsMgr/users/ta/home.php');
+        header('Location: /COMP-5531---CrsMgr-/users/ta/home.php');
         exit;
     } elseif ($_POST['role'] == 'Student') {
         // Redirect to the Student home page
-        header('Location: /Final_Assignment_CrsMgr/users/student/home.php');
+        header('Location: /COMP-5531---CrsMgr-/users/student/home.php');
         exit;
     }
 }
 
 // Redirect to the login page if no role is selected or if the form wasn't submitted
-header('Location: /Final_Assignment_CrsMgr/login.php');
+header('Location: /COMP-5531---CrsMgr-/login.php');
 exit;
 ?>
