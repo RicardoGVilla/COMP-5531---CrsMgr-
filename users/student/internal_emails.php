72fa74ee2174f79b85ae488307234383bd942316
Omar Ghandour <omar.z.ghandour@gmail.com>
2024-04-17 16:18:10 -0400

    Test Fix

--- users/student/internal_emails.php
+++ users/student/internal_emails.php
@@ -173,9 +173,10 @@ switch ($action) {
         <!-- Sidebar -->
         <div class="sidebar">
             <!-- Sidebar buttons -->
-            <button onclick="location.href='manage_courses.php'">Manage Courses</button>
-            <button onclick="location.href='manage_student_groups.php'">Manage Student Groups</button>
-            <button onclick="location.href='manage_faqs.php'">Manage FAQs</button>
+            <button onclick="location.href='contact_information.php'">Contact Information</button>
+            <button onclick="location.href='faq-information.php'">FAQ</button>
+            <button onclick="location.href='group-information.php'">My Group Information </button>
+            <button onclick="location.href='manage-files.php'"> Manage Files </button>
             <button onclick="location.href='internal_emails.php'">Email</button>
         </div>
 
