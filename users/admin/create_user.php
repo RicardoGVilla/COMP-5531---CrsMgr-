5dff89c31d2114cb0d4a997b8c2fdafa08ec256c
Ricardo <ricardorgvillanueva@gmail.com>
2024-04-17 17:58:19 -0400

    fixing log out button

--- users/admin/create_user.php
+++ users/admin/create_user.php
@@ -36,15 +36,17 @@ usort($users, 'compareUsers');
             <h1>Welcome Admin</h1>
         </header>
 
+        <!-- Sidebar Navigation -->
         <div class="sidebar">
-            <button onclick="location.href='create_user.php'">Manage Users</button>
+        <button onclick="location.href='create_user.php'">Manage Users</button>
+            <button onclick="location.href='manage_user.php'">Manage Roles</button>
             <button onclick="location.href='manage_courses.php'">Manage Courses</button>
             <button onclick="location.href='manage_sections.php'">Manage Sections</button>
             <button onclick="location.href='manage_groups.php'">Manage Groups</button>
             <button onclick="location.href='manage_announcements.php'">Course Announcements</button>
             <button onclick="location.href='manage_faqs.php'">FAQ Management</button>
+            <button onclick="location.href='enrolling_students.php'">Course Enrollment</button>
         </div>
-        
         <main class="main">
             <h2>Manage Users</h2>
 
@@ -106,7 +108,7 @@ usort($users, 'compareUsers');
 
         <footer class="footer">
             <button onclick="location.href='home.php'">Home</button>
-            <button onclick="location.href='logout.php'">Logout</button>
+            <button onclick="location.href='../../logout.php'">Logout</button>
         </footer>
     </div>
 
